1d75cf1f9310aa8aee391677512ec184243f74c5
HannaGFelix <hannag.felix@gmail.com>
2023-07-18 18:57:59 -0400

    currently editing

--- app/src/main/java/algonquin/cst2335/finalandroidproject/Currency/CurrencyConverterActivity.java
+++ app/src/main/java/algonquin/cst2335/finalandroidproject/Currency/CurrencyConverterActivity.java
@@ -11,4 +11,30 @@ public class CurrencyConverterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_converter);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
