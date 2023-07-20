ccbdd760e8763fc16065f1683f976cc3058ac3ed
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2023-07-20 15:58:40 -0400

    added DAO, added DB and sharedPref(not working)

--- app/src/main/java/algonquin/cst2335/finalandroidproject/Aviation/AviationTrackerActivity.java
+++ app/src/main/java/algonquin/cst2335/finalandroidproject/Aviation/AviationTrackerActivity.java
@@ -2,37 +2,125 @@ package algonquin.cst2335.finalandroidproject.Aviation;
 
 import static java.lang.Character.isDigit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.zip.Inflater;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import algonquin.cst2335.finalandroidproject.Aviation.DAO.FlightRequestDAO;
+import algonquin.cst2335.finalandroidproject.Aviation.Data.AviationTrackerViewModel;
+import algonquin.cst2335.finalandroidproject.Aviation.Data.FlightRequest;
+import algonquin.cst2335.finalandroidproject.Aviation.Data.FlightRequestDB;
 import algonquin.cst2335.finalandroidproject.R;
 import algonquin.cst2335.finalandroidproject.databinding.ActivityAviationTrackerBinding;
+import algonquin.cst2335.finalandroidproject.databinding.ActivityGetFlightDataBinding;
 
 public class AviationTrackerActivity extends AppCompatActivity {
 
+    private static  final String SHARED_PREF = "Data";
+    private static final String REQ_CODE = "Code";
+    private  RecyclerView.Adapter myAdapter;
     private ActivityAviationTrackerBinding binding;
+    private FlightRequestDAO DAO;
+    private ArrayList<FlightRequest> requests;
+    private FlightRequest newReq;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        sharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+//        String savedCode = sharedPreferences.getString(REQ_CODE,"");
+//        binding.editTextText.setText(savedCode);
+
         super.onCreate(savedInstanceState);
 
 
+
+        AviationTrackerViewModel ATViewModel = new ViewModelProvider(this).get(AviationTrackerViewModel.class);
+        requests = ATViewModel.requests.getValue();
+        FlightRequestDB db = Room.databaseBuilder(getApplicationContext(), FlightRequestDB.class, "FlightReqDB").build();
+        DAO = db.frDAO();
+        if (requests == null) {
+
+            ATViewModel.requests.setValue(requests = new ArrayList<>());
+            Executor thread = Executors.newSingleThreadExecutor();
+            thread.execute(() ->
+            {
+                requests.addAll(DAO.getAllMessage());
+                runOnUiThread(() -> binding.recycleView.setAdapter(myAdapter));
+            });
+
+        }
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+        String savedCode = sharedPreferences.getString(REQ_CODE,"");
+        binding.editTextText.setText(savedCode);
         binding = ActivityAviationTrackerBinding.inflate(getLayoutInflater());
+
+
         setContentView(binding.getRoot());
         binding.button.setOnClickListener(click ->{
             String typed = binding.editTextText.getText().toString();
+            newReq = new FlightRequest(typed);
+            if(checkCode(typed)){
+                requests.add(newReq);
+                myAdapter.notifyItemInserted(requests.size()-1);
+                binding.editTextText.setText("");
+                Executor thread = Executors.newSingleThreadExecutor();
+                thread.execute(() -> newReq.id = (int) DAO.insertCode(newReq));
 
-            if(checkCode(typed) == true){
-            }else {
+                String code = binding.editTextText.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(REQ_CODE,code);
+                editor.apply();
             }
         });
+
+        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                ActivityGetFlightDataBinding binding = ActivityGetFlightDataBinding.inflate(getLayoutInflater(), parent, false);
+                return new MyRowHolder(binding.getRoot());
+
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+                holder.codeText.setText("");
+
+                FlightRequest obj = requests.get(position);
+                holder.codeText.setText(obj.getCode());
+            }
+
+            @Override
+            public int getItemCount() {
+                return requests.size();
+            }
+            
+        });
     }
+
+
     //if the amount of capital letters do not match the length, show error in typed code.
-    boolean checkCode(String cd) {
+    boolean checkCode(@NonNull String cd) {
         int duration = Toast.LENGTH_SHORT;
         int counter = 0;
         char c;
@@ -55,5 +143,38 @@ public class AviationTrackerActivity extends AppCompatActivity {
             Toast.makeText(this, "Code is not valid, 3 letters please", duration).show();
             return false;
         }
+    }
+
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView codeText;
+
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(clk -> {
+                int position = getAbsoluteAdapterPosition();
+                AlertDialog.Builder builder = new AlertDialog.Builder(AviationTrackerActivity.this);
+                builder.setTitle("Question:")
+                        .setMessage("Do you want to delete the dataset: " + codeText.getText())
+                        .setNegativeButton("No", (dialog, cl) -> {
+                        })
+                        .setPositiveButton("Yes", (dialog, cl) -> {
+                            Executor thread = Executors.newSingleThreadExecutor();
+                            FlightRequest m = requests.get(position);
+                            thread.execute(() -> DAO.deleteCode(m));
+                            requests.remove(position);
+                            myAdapter.notifyItemRemoved(position);
+                            Snackbar.make(codeText, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", click -> {
+                                        requests.add(position, m);
+                                        runOnUiThread(() -> myAdapter.notifyItemInserted(position));
+                                    })
+                                    .show();
+                        })
+                        .create().show();
+            });
+            codeText = itemView.findViewById(R.id.textView2);
+        }
+
+
     }
 }
--- app/src/main/java/algonquin/cst2335/finalandroidproject/Aviation/Data/FlightRequest.java
+++ app/src/main/java/algonquin/cst2335/finalandroidproject/Aviation/Data/FlightRequest.java
@@ -1,7 +1,32 @@
 package algonquin.cst2335.finalandroidproject.Aviation.Data;
 
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class FlightRequest {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    public int id;
+
+    @ColumnInfo(name= "reqCode")
+    protected String Code;
 
     public FlightRequest(){};
+    public FlightRequest(String m){
+
+        Code = m;
+    };
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
 
 }
